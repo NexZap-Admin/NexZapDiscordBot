86f3d8005a821d18bd071f1a3a1a8804e29368b3
ThuongDev <cuong.0907062600@gmail.com>
2026-06-03 14:29:52 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+class Program
+{
+    private DiscordSocketClient _client;
+
+    static Task Main()
+        => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient(new DiscordSocketConfig
+        {
+            GatewayIntents =
+                GatewayIntents.Guilds |
+                GatewayIntents.GuildMembers
+        });
+
+        _client.Log += Log;
+        _client.UserJoined += UserJoined;
+
+        string token = "BOT_TOKEN_CUA_BAN";
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private async Task UserJoined(SocketGuildUser user)
+    {
+        var channel = user.Guild.TextChannels
+            .FirstOrDefault(x => x.Name == "welcome");
+
+        if (channel != null)
+        {
+            await channel.SendMessageAsync(
+                 $"🎉 Chào mừng bạn {user.Username} đã gia nhập gia đình NexZap."
+            );
+        }
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg);
+        return Task.CompletedTask;
+    }
+}
